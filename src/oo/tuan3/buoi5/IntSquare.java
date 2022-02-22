706fe92fecb1428a22bb34351800eec3d2493a67
lhphuoc2311 <phuoc2311@gmail.com>
2022-02-22 17:14:51 +0700

    source code Int Square no Abstraction

---
+++ src/oo/tuan3/buoi5/IntSquare.java
@@ -0,0 +1,17 @@
+/*
+*  created date: Feb 22, 2022
+*  author: cgm
+*/
+package oo.tuan3.buoi5;
+
+public class IntSquare {
+
+    private double squareValue;//implementation
+
+    //interface
+    public double getSquareOperator(int value){
+        this.squareValue = value * value;//operator X
+        return this.squareValue;
+    }
+    
+}
---
+++ src/oo/tuan3/buoi5/IntSquareTestDrive.java
@@ -0,0 +1,15 @@
+/*
+*  created date: Feb 22, 2022
+*  author: cgm
+*/
+package oo.tuan3.buoi5;
+
+public class IntSquareTestDrive {
+
+    public static void main(String[] args) {
+        IntSquare intSquare = new IntSquare();
+        double result  = intSquare.getSquareOperator(2);
+        System.out.println(result);
+    }
+    
+}
